1fd607481dbe95f210beaf360260c9c38b4c67aa
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-25 15:18:04 +0100

    Feat → added img to JS and HTML using Vue's logic & added (ง•̀_•́)ง

--- js/app.js
+++ js/app.js
@@ -6,8 +6,19 @@ const { createApp } = Vue
 createApp({
   data() {
     return{
+        // contenuto del titolo
         title:'Hello, Vue is now linked to this App!',
+        // classi del titolo
         titleClasses: "text-center main-title",
+
+        // (ง•̀_•́)ง
+        lessgo: "(ง•̀_•́)ง",
+        lessgoStyle: "text-center lessgo",
+
+        // link per l'immagine
+        imgSrc: "./img/ChibiFlayer.png",
+        // classi relative all'immagine
+        imgClasses:"text-center mt-5",
     }
   },
 }).mount('#app')
